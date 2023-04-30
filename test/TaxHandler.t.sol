0584a7ab9cb6b3056cc951fad71c1294dadac406
0xvalerius <0xvalerius@proton.me>
2023-04-30 23:28:34 +0200

    Added test for buy and sell fees.

--- test/TaxHandler.t.sol
+++ test/TaxHandler.t.sol
@@ -30,7 +30,11 @@ contract MockTokenTest is Test {
         token = new MockToken('MockToken', 'MTK', treasury, transferFee, buyFee,
         sellFee);
         token.addWhitelist(treasury);
+
+        // set liqiduity pair for buy / sell fees
+        token.addLiquidityPair(liquidityPair);
         vm.stopPrank();
+
         deal(address(token), owner, initialTokenActorBalance, true);
         deal(address(token), actor1, initialTokenActorBalance, true);
         deal(address(token), actor2, initialTokenActorBalance, true);
@@ -45,8 +49,9 @@ contract MockTokenTest is Test {
         assertEq(token.basisPointsFee(1), buyFee);
         assertEq(token.basisPointsFee(2), sellFee);
         assertEq(token.totalSupply(), 4 * initialTokenActorBalance);
-        assertEq(token.isTaxWhitelisted(owner), true);
-        assertEq(token.isTaxWhitelisted(treasury), true);
+        assertEq(token.isFeeWhitelisted(owner), true);
+        assertEq(token.isFeeWhitelisted(treasury), true);
+        assertEq(token.isLiquidityPair(liquidityPair), true);
     }
 
     function test_setTreasury() public {
@@ -92,13 +97,19 @@ contract MockTokenTest is Test {
 
         vm.startPrank(owner);
         token.addWhitelist(address(0x1234));
-        assertEq(token.isTaxWhitelisted(address(0x1234)), true);
+        assertEq(token.isFeeWhitelisted(address(0x1234)), true);
         token.removeWhitelist(address(0x1234));
         vm.stopPrank();
-        assertEq(token.isTaxWhitelisted(address(0x1234)), false);
+        assertEq(token.isFeeWhitelisted(address(0x1234)), false);
     }
 
-    function test_getFeeRate() public {}
+    function test_getFeeRate() public {
+        assertEq(token.getFeeRate(owner, actor1), 0);
+        assertEq(token.getFeeRate(actor1, owner), 0);
+        assertEq(token.getFeeRate(actor1, actor2), transferFee);
+        assertEq(token.getFeeRate(liquidityPair, actor1), buyFee);
+        assertEq(token.getFeeRate(actor1, liquidityPair), sellFee);
+    }
 
     function test_WhiteListedTransfer() public {
         // transfer from owner to actor1
@@ -116,12 +127,44 @@ contract MockTokenTest is Test {
         assertEq(token.balanceOf(treasury), 0);
     }
 
-    function test_TaxOnTransfer() public {
+    function test_UserToUserTransfer() public {
         // transfer from actor1 to actor2
         vm.prank(actor1);
         token.transfer(actor2, 1000);
         assertEq(token.balanceOf(actor1), initialTokenActorBalance - 1000);
         assertEq(token.balanceOf(actor2), initialTokenActorBalance + 990);
         assertEq(token.balanceOf(treasury), 10);
+    }
+
+    function test_BuyTransfer() public {
+        // actor1 buys
+        vm.prank(liquidityPair);
+        token.transfer(actor1, 1000);
+        assertEq(token.balanceOf(liquidityPair), initialTokenActorBalance - 1000);
+        assertEq(token.balanceOf(actor1), initialTokenActorBalance + 980);
+        assertEq(token.balanceOf(treasury), 20);
+
+        // whitelisted address buys
+        vm.prank(liquidityPair);
+        token.transfer(owner, 1000);
+        assertEq(token.balanceOf(liquidityPair), initialTokenActorBalance - 2000);
+        assertEq(token.balanceOf(owner), initialTokenActorBalance + 1000);
+        assertEq(token.balanceOf(treasury), 20);
+    }
+
+    function test_SellTransfer() public {
+        // actor2 sells
+        vm.prank(actor2);
+        token.transfer(liquidityPair, 1000);
+        assertEq(token.balanceOf(liquidityPair), initialTokenActorBalance + 970);
+        assertEq(token.balanceOf(actor2), initialTokenActorBalance - 1000);
+        assertEq(token.balanceOf(treasury), 30);
+
+        // whitelisted address sells
+        vm.prank(owner);
+        token.transfer(liquidityPair, 1000);
+        assertEq(token.balanceOf(liquidityPair), initialTokenActorBalance + 1970);
+        assertEq(token.balanceOf(owner), initialTokenActorBalance - 1000);
+        assertEq(token.balanceOf(treasury), 30);
     }
 }
