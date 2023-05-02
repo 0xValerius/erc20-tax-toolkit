4bf8a5e54d99ec47f48ad8705a9e0ef67343fab1
0xvalerius <0xvalerius@proton.me>
2023-05-03 00:05:01 +0200

    Fixed bug in TransferLimiter.sol.

--- src/contracts/TransferLimiter.sol
+++ src/contracts/TransferLimiter.sol
@@ -6,7 +6,6 @@ import {ERC20} from "openzeppelin/token/ERC20/ERC20.sol";
 import {Ownable} from "openzeppelin/access/Ownable.sol";
 
 abstract contract TransferLimiter is ERC20, Ownable {
-    // two decimal resolution
     uint256 public constant TRANSFER_LIMIT_DENOMINATOR = 10000;
 
     uint256 public basePointsTransferLimit;
@@ -15,24 +14,24 @@ abstract contract TransferLimiter is ERC20, Ownable {
         basePointsTransferLimit = _basePointsTranferLimit;
     }
 
-    mapping(address => bool) public isTransferWhitelisted;
+    mapping(address => bool) public isTransferLimitWhitelisted;
 
     function addTransferLimit(address _address) external onlyOwner {
-        isTransferWhitelisted[_address] = true;
+        isTransferLimitWhitelisted[_address] = true;
     }
 
     function removeTransferLimit(address _address) external onlyOwner {
-        isTransferWhitelisted[_address] = false;
+        isTransferLimitWhitelisted[_address] = false;
+    }
+
+    function allowedAmount() public view returns (uint256) {
+        return (totalSupply() * basePointsTransferLimit) / TRANSFER_LIMIT_DENOMINATOR;
     }
 
     function _beforeTokenTransfer(address from, address to, uint256 amount) internal virtual override {
         super._beforeTokenTransfer(from, to, amount);
-        if (!isTransferWhitelisted[from] && !isTransferWhitelisted[to]) {
-            uint256 balance = balanceOf(from);
-            require(
-                amount <= (totalSupply * basePointsTransferLimit) / TRANSFER_LIMIT_DENOMINATOR,
-                "TransferLimiter: transfer amount exceeds limit"
-            );
+        if (!isTransferLimitWhitelisted[from] && !isTransferLimitWhitelisted[to]) {
+            require(amount <= allowedAmount(), "TransferLimiter: transfer amount exceeds limit");
         }
     }
 }
