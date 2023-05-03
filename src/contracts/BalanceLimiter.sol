5511f2c64e8275f96525bf307cc4f95dab6d714a
0xvalerius <0xvalerius@proton.me>
2023-05-03 22:59:07 +0200

    Added BalanceLimiter.sol.

---
+++ src/contracts/BalanceLimiter.sol
@@ -0,0 +1,36 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.17;
+
+import {ERC20} from "openzeppelin/token/ERC20/ERC20.sol";
+import {Ownable} from "openzeppelin/access/Ownable.sol";
+
+abstract contract BalanceLimiter is ERC20, Ownable {
+    uint256 public constant BALANCE_LIMIT_DENOMINATOR = 10000;
+    uint256 public basePointsBalanceLimit;
+    mapping(address => bool) public isBalanceLimitWhitelisted;
+
+    constructor(uint256 _basePointsBalanceLimit) {
+        basePointsBalanceLimit = _basePointsBalanceLimit;
+        isBalanceLimitWhitelisted[msg.sender] = true;
+    }
+
+    function addBalanceLimit(address _address) external onlyOwner {
+        isBalanceLimitWhitelisted[_address] = true;
+    }
+
+    function removeBalanceLimit(address _address) external onlyOwner {
+        isBalanceLimitWhitelisted[_address] = false;
+    }
+
+    function allowedMaxBalance() public view returns (uint256) {
+        return (totalSupply() * basePointsBalanceLimit) / BALANCE_LIMIT_DENOMINATOR;
+    }
+
+    function _beforeTokenTransfer(address from, address to, uint256 amount) internal virtual override {
+        super._beforeTokenTransfer(from, to, amount);
+        if (!isBalanceLimitWhitelisted[from] && !isBalanceLimitWhitelisted[to]) {
+            require(amount <= allowedMaxBalance(), "TransferLimiter: transfer amount exceeds limit");
+        }
+    }
+}
